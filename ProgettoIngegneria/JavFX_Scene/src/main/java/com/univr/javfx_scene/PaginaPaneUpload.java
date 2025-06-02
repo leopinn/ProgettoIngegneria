2489cf288ea07d0f1743e17006153bab98e25d89
Pinna2003 <pinna558@gmail.com>
2025-06-02 13:36:43 +0200

    Progetto_20250602_2
    - Inizializzata PaginaUpload
    - Inseriti file per la musica

--- ProgettoIngegneria/JavFX_Scene/src/main/java/com/univr/javfx_scene/PaginaPaneUpload.java
+++ ProgettoIngegneria/JavFX_Scene/src/main/java/com/univr/javfx_scene/PaginaPaneUpload.java
@@ -3,7 +3,9 @@ package com.univr.javfx_scene;
 import java.io.IOException;
 
 public class PaginaPaneUpload {
+    private  ObjSql objSql = ObjSql.oggettoSql();
     private PaginaPrincipale mainController; // Importante per permettere il cambio dei vari pane nella pagina principale
+
 
     public void setMainController(PaginaPrincipale controller) {
         this.mainController = controller;
